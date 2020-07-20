cebb06adfa3431ff576bb421dbbb009ed736b121
mrbluesky0123 <onightraino@gmail.com>
2020-07-20 17:48:42 +0900

    asdasd

--- src/main/java/com/skcc/minicommerce/Things/controller/ThingsController.java
+++ src/main/java/com/skcc/minicommerce/Things/controller/ThingsController.java
@@ -2,11 +2,12 @@ package com.skcc.minicommerce.Things.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ThingsController {
 
-  @GetMapping("/things/getthingsinfo")
+  @GetMapping("/things/thingsinfo")
   public String getThingsInfo(){
     return "Things information.";
   }
