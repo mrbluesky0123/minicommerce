d3aa650cdc603209115f1e2a5eee859880bd51a6
mrbluesky0123 <onightraino@gmail.com>
2020-07-22 08:44:47 +0900

    Asdasd

--- src/main/java/com/skcc/minicommerce/goods/controller/GoodsController.java
+++ src/main/java/com/skcc/minicommerce/goods/controller/GoodsController.java
@@ -3,10 +3,12 @@ package com.skcc.minicommerce.goods.controller;
 import com.skcc.minicommerce.goods.dto.GoodsDto;
 import com.skcc.minicommerce.goods.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="*")
 @RestController
 public class GoodsController {
 
